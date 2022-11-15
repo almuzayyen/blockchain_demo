2cdcafc6689b4a6f4b808dbc177977c1d9188a28
Mohammed Abdullatif Al Muzayyen | Ejada Systems Ltd <malmuzayyen@EjadaDMM.Local>
2022-11-15 11:22:50 +0300

    add

---
+++ blockchain_client/blockchain_client.py
@@ -0,0 +1,6 @@
+class Tarnsaction:
+    def __init__ (self,sender_address,sender_private_key,receipient_address,value):
+        self.sender_address = sender_address
+        self.sender_private_key = sender_private_key
+        self.receipient_address = receipient_address
+        self.value = value
